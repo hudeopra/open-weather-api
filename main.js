e6237485044d7d91c4157d6d25a51b9440d04b1a
Prabin Maharjan <maharjanhudeo@gmail.com>
2023-07-13 15:39:42 +0545

    Final Submittion
    Phase 1 Complete

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 const apikey = "eb00a5c5306a122e858cd765a9c56429";
 var currentLocation = `east+ayrshire`;
 let url = `https://api.openweathermap.org/data/2.5/weather?&units=metric`;
+const weatherIcon = document.getElementById("ph-main__weather-icon");
 // feching the data from the API URL
 async function getdate() {
     const date = new Date()
@@ -40,7 +41,7 @@ async function getdate() {
         }
     }
     var year = date.getFullYear()
-    document.getElementById("ph-date").innerHTML = ` ${day}${dayFormat(day)} ${getMonthName(month)} ${year}`;
+    document.getElementById("ph-date").innerHTML = ` ${day}${dayFormat(day)} ${getMonthName(month)}, ${year}`;
 
 }
 function dayFormat(day) {
@@ -59,15 +60,48 @@ async function weatherCheck(city) {
     const response = await fetch(url + `&appid=${apikey}` + `&q=` + city);
     var data = await response.json();
     console.log(data);
+    if (data.cod === "404") {
+        alert("City not found");
+        return;
+    }
     console.log(`Current Location: ${data.name}`);
+
     document.getElementById("ph-current-weather").innerHTML = `${data.weather[0].main}`
 
     document.getElementById("ph-current-location").innerHTML = `${data.name}`
+    // document.getElementById("ph-history-location").innerHTML = `${data.name}`
     document.getElementById("ph-current-temp").innerHTML = `${data.main.temp} `
 
     document.getElementById("humidity").innerHTML = `${data.main.humidity} `
     document.getElementById("air-pressure").innerHTML = `${data.main.pressure} `
     document.getElementById("wind-speed").innerHTML = `${data.wind.speed} `
+
+
+    if (data.weather[0].main == "Clear" || data.weather[0].main == "Clear Sky") {
+        weatherIcon.src = "img/icon/clear.png";
+    } else if (data.weather[0].main == "Clouds" || data.weather[0].main == "Few Clouds" || data.weather[0].main == "Scattered Clouds" || data.weather[0].main == "Broken Clouds") {
+        weatherIcon.src = "img/icon/cloudy.png";
+    } else if (data.weather[0].main == "Rain" || data.weather[0].main == "Shower Rain" || data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "img/icon/rainy.png";
+    } else if (data.weather[0].main == "Thunderstorm") {
+        weatherIcon.src = "img/icon/thunderstorm.png";
+    } else if (data.weather[0].main == "Snow") {
+        weatherIcon.src = "img/icon/snow.png";
+    } else if (data.weather[0].main == "Mist" || data.weather[0].main == "Fog") {
+        weatherIcon.src = "img/icon/mist.png";
+    } else if (data.weather[0].main == "Haze" || data.weather[0].main == "Smoke") {
+        weatherIcon.src = "img/icon/haze.png";
+    } else if (data.weather[0].main == "Dust" || data.weather[0].main == "Sand") {
+        weatherIcon.src = "img/icon/dust.png";
+    } else if (data.weather[0].main == "Ash") {
+        weatherIcon.src = "img/icon/ash.png";
+    } else if (data.weather[0].main == "Tornado") {
+        weatherIcon.src = "img/icon/tornado.png";
+    } else {
+        // Default image if the weather condition is not recognized
+        weatherIcon.src = "img/icon/default.png";
+    }
+
 }
 weatherCheck(currentLocation)
 
@@ -92,25 +126,10 @@ searchForm.addEventListener("submit", validateForm);
 
 // Access searchCity variable outside of the function
 console.log(searchCity);
-// weatherCheck(searchCity)
-// fetch(url)
-//     // .then is a promice which flows only sucessful events
-//     .then((response) => response.json())
-//     .then((data) => {
-//         console.log(data);
-//         console.log(data.name);
-//         document.getElementById("ph-current-weather").innerHTML = `${ data.weather[0].main } `
 
-//         document.getElementById("ph-current-location").innerHTML = `${ data.name } `
-//         document.getElementById("ph-current-temp").innerHTML = `${ data.main.temp } `
+$(document).on('click', '#ph-search-trigger', function () {
+    $('#ph-search-trigger').toggleClass('active');
+});
+// icon remaining
 
-//         document.getElementById("humidity").innerHTML = `${ data.main.humidity } `
-//         document.getElementById("air-pressure").innerHTML = `${ data.main.pressure } `
-//         document.getElementById("ph-current-temp").innerHTML = `${ data.main.temp } `
-//         document.getElementById("wind-speed").innerHTML = `${ data.wind.speed } `
-
-//     });
-
-// no date
-// no chance of rain
-// no icon update
+// history html and css fix
