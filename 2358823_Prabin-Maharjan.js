ffc8c7e8a963896aea9797438d3f85aed73e2648
Prabin Maharjan <maharjanhudeo@gmail.com>
2023-07-27 21:05:22 +0545

    Add files via upload

--- 2358823_Prabin-Maharjan.js
+++ 2358823_Prabin-Maharjan.js
@@ -1,12 +1,13 @@
 // Name: Prabin Maharjan
 // UID: 2358823
 
-// declearing variables using const, var and let
-
 const apikey = "eb00a5c5306a122e858cd765a9c56429";
 var currentLocation = `east+ayrshire`;
 let url = `https://api.openweathermap.org/data/2.5/weather?&units=metric`;
 const weatherIcon = document.getElementById("ph-main__weather-icon");
+
+
+
 // feching the data from the API URL using async function
 async function getdate() {
     // Date() is a built in JS function and storing its value in date  variable
@@ -68,95 +69,191 @@ function dayFormat(day) {
 // calling getdate() function
 getdate()
 
-//declaring a asynchronous function that fetches weather data from an API based on the provided city parameter.
-async function weatherCheck(city) {
-    // Await  keyword paused the until a promice is fulfilled or resolved
-    const response = await fetch(url + `&appid=${apikey}` + `&q=` + city);
-    // declearing a variable where Object returned form API is stored 
-    var data = await response.json();
-    // Printing the data in console
-    console.log(`Current Location: ${data.name}`);
-    console.log(data);
-    // in case of invalid city name or its data alert user 
-    if (data.cod === "404") {
-        alert("City not found");
-        return;
-    }
-    //HTML element with their respective ID is being updated with content from API object. 
-    document.getElementById("ph-current-weather").innerHTML = `${data.weather[0].main}`
-
-    document.getElementById("ph-current-location").innerHTML = `${data.name}`
-    document.getElementById("ph-current-temp").innerHTML = `${data.main.temp} `
-
-    document.getElementById("humidity").innerHTML = `${data.main.humidity} `
-    document.getElementById("air-pressure").innerHTML = `${data.main.pressure} `
-    document.getElementById("wind-speed").innerHTML = `${data.wind.speed} `
+// Function to update weather data and display on the webpage
+function updateWeatherData(data) {
+    document.getElementById("ph-current-weather").innerHTML = data.weather[0].main;
+    document.getElementById("ph-current-location").innerHTML = data.name;
+    document.getElementById("ph-current-temp").innerHTML = `${data.main.temp} `;
+    document.getElementById("humidity").innerHTML = `${data.main.humidity} `;
+    document.getElementById("air-pressure").innerHTML = `${data.main.pressure} `;
+    document.getElementById("wind-speed").innerHTML = `${data.wind.speed} `;
 
 
     // conditional statements 
     // based on the data received from API source URL in the image tag with ID "ph-main__weather-icon" being updated or changed
     if (data.weather[0].main == "Clear" || data.weather[0].main == "Clear Sky") {
-        weatherIcon.src = "img/icon/clear.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/clear.png";
     } else if (data.weather[0].main == "Clouds" || data.weather[0].main == "Few Clouds" || data.weather[0].main == "Scattered Clouds" || data.weather[0].main == "Broken Clouds") {
-        weatherIcon.src = "img/icon/cloudy.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/cloudy.png";
     } else if (data.weather[0].main == "Rain" || data.weather[0].main == "Shower Rain" || data.weather[0].main == "Drizzle") {
-        weatherIcon.src = "img/icon/rainy.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/rainy.png";
     } else if (data.weather[0].main == "Thunderstorm") {
-        weatherIcon.src = "img/icon/thunderstorm.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/thunderstorm.png";
     } else if (data.weather[0].main == "Snow") {
-        weatherIcon.src = "img/icon/snow.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/snow.png";
     } else if (data.weather[0].main == "Mist" || data.weather[0].main == "Fog") {
-        weatherIcon.src = "img/icon/mist.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/mist.png";
     } else if (data.weather[0].main == "Haze" || data.weather[0].main == "Smoke") {
-        weatherIcon.src = "img/icon/haze.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/haze.png";
     } else if (data.weather[0].main == "Dust" || data.weather[0].main == "Sand") {
-        weatherIcon.src = "img/icon/dust.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/dust.png";
     } else if (data.weather[0].main == "Ash") {
-        weatherIcon.src = "img/icon/ash.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/ash.png";
     } else if (data.weather[0].main == "Tornado") {
-        weatherIcon.src = "img/icon/tornado.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/tornado.png";
     } else {
         // Default image if the weather condition is not recognized
-        weatherIcon.src = "img/icon/default.png";
+        weatherIcon.src = "https://hudeopra.github.io/open-weather-api/img/icon/default.png";
     }
 
 }
-// calling or executing the function by passing the variable "currentLocation"
-weatherCheck(currentLocation)
 
-// declearing a variable without assigning value
-let searchCity;
+// Function to fetch weather data from the API
+async function weatherCheck(city) {
+    try {
+        const response = await fetch(url + `&appid=${apikey}` + `&q=` + city);
+        const data = await response.json();
+        console.log(data)
+        if (data.cod === "404") {
+            alert("City not found");
+            return;
+        }
+
+        updateWeatherData(data);
+
+        // Convert the weatherData object to JSON string
+        const jsonData = JSON.stringify(data);
+
+        // Log the JSON data in the console (optional)
+        console.log(jsonData);
+
+        // Store the JSON data on the server using AJAX
+        await storeDataOnServer(jsonData);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+// Function to store the JSON data on the server using AJAX
+async function storeDataOnServer(jsonData) {
+    try {
+        // Validate JSON data
+        const parsedData = JSON.parse(jsonData);
+        if (!parsedData) {
+            throw "Invalid JSON data";
+        }
+
+        // Send an AJAX POST request to the PHP script to store the data
+        const response = await fetch('http://localhost/weather/storedata.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonData // Send JSON data directly without wrapping it inside 'jsonData'
+        });
+
+        // Check the response status
+        if (!response.ok) {
+            throw "Error storing data on the server";
+        }
+
+        // Parse the response
+        const result = await response.json();
+
+        // Log the result in the console (optional)
+        console.log(result);
+
+        // Update the data.json file with the latest JSON data
+        await updateDataFile(jsonData); // Call the function to update the data.json file
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Function to update the data.json file with the latest JSON data
+async function updateDataFile(jsonData) {
+    try {
+        // Send an AJAX POST request to the PHP script to update the data.json file
+        const response = await fetch('http://localhost/weather/updateDataFile.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonData
+        });
+
+        // Check the response status
+        if (!response.ok) {
+            throw "Error updating data.json file";
+        }
+
+        // Parse the response
+        const result = await response.json();
+
+        // Log the result in the console (optional)
+        console.log(result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+// Function to execute when the page loads
+async function onPageLoad() {
+    const searchForm = document.forms.search;
+
+    // Adds an event listener to the form element with the ID "search" for the "submit" event.
+    // When the form is submitted, the validateForm function is called.
+    searchForm.addEventListener("submit", validateForm);
+
+    // Adds a click event listener to the entire document (page) using jQuery's $(document).on() function.
+    // The event listener is triggered when a click event occurs on an element with the ID "ph-search-trigger".
+    // It is an anonymous function.
+    $(document).on('click', '#ph-search-trigger', function () {
+        // toggle class named "active" every time the function is triggered
+        $('#ph-search-trigger').toggleClass('active');
+    });
+
+    await weatherCheck(currentLocation);
+}
+
+// Declare the searchForm variable outside the onPageLoad function
 const searchForm = document.forms.search;
 
+// Function to validate the search form
 function validateForm(e) {
     // Prevents the default form submission behavior
     e.preventDefault();
+
     // Retrieves the value entered in the input field with the name "cityName"
-    searchCity = searchForm.cityName.value;
+    const searchCity = searchForm.cityName.value;
+
     try {
         // Checks if the searchCity variable is empty
         if (!searchCity) throw "The search box is empty";
     } catch (err) {
         // Displays an alert message if the searchCity variable is empty
         alert(err);
+        return;
     }
-    // Assigns/updated the value of searchCity to the currentLocation variable
-    currentLocation = searchCity;
-    // Calls the weatherCheck function with the updated currentLocation value
-    weatherCheck(currentLocation);
+
+    // Calls the weatherCheck function with the updated searchCity value
+    weatherCheck(searchCity);
 }
+
+// Execute onPageLoad function when the page finishes loading
+window.addEventListener('load', onPageLoad);
 
 // Adds an event listener to the form element with the ID "search" for the "submit" event.
 // When the form is submitted, the validateForm function is called.
 searchForm.addEventListener("submit", validateForm);
 
 
-
 // Adds a click event listener to the entire document (page) using jQuery's $(document).on() function.
 // The event listener is triggered when a click event occurs on an element with the ID "ph-search-trigger".
 // it is an anonymous function
-$(document).on('click', '#ph-search-trigger', function () {
+$(document).on('click', '.ph-history #ph-search-trigger', function () {
     // toggle class named "active" everytime the function is triggred 
     $('#ph-search-trigger').toggleClass('active');
 });
+
 
---
+++ storedata.php
@@ -0,0 +1,128 @@
+<?php
+// Allow CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// MySQL credentials
+$servername = "localhost";
+$username = "prabinhudeo";
+$password = "hudeop";
+$dbname = "weather_webapp";
+
+// Function to retrieve and display weather data
+function displayWeatherData($locationName, $conn) {
+    // Prepare the SQL statement to retrieve the 7 most recent weather data for the given location
+    $sql = "SELECT * FROM weather_data
+            WHERE location = ? 
+            ORDER BY date DESC
+            LIMIT 7";
+
+    // Prepare the statement
+    $stmt = $conn->prepare($sql);
+
+    // Bind the parameter
+    $stmt->bind_param("s", $locationName);
+
+    // Execute the statement
+    $stmt->execute();
+
+    // Get the result
+    $result = $stmt->get_result();
+
+    // Check if there are any results
+    if ($result->num_rows > 0) {
+        // Initialize an array to keep track of displayed dates
+        $displayedDates = array();
+
+        // Output the data
+        while ($row = $result->fetch_assoc()) {
+            $date = date('Y-m-d', strtotime($row['date'])); // Convert the date to Y-m-d format
+            // Check if the date has already been displayed
+            if (!in_array($date, $displayedDates)) {
+                // Add the date to the displayedDates array
+                $displayedDates[] = $date;
+
+                echo "Date: " . $row['date'] . "<br>";
+                echo "Location Name: " . $row['location'] . "<br>";
+                echo "Weather Main: " . $row['weather_main'] . "<br>";
+                echo "Temperature: " . $row['temperature'] . "<br>";
+                echo "Humidity: " . $row['humidity'] . "<br>";
+                echo "Pressure: " . $row['pressure'] . "<br>";
+                echo "Wind Speed: " . $row['wind_speed'] . "<br>";
+                echo "<hr>";
+            }
+        }
+    } else {
+        echo "No data found for the location: " . $locationName;
+    }
+
+    // Close the statement
+    $stmt->close();
+}
+
+// Check if the request method is POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the JSON data from the POST request body
+    $jsonData = file_get_contents('php://input');
+
+    try {
+        // Check if JSON data exists
+        if (!empty($jsonData)) {
+            // Write the JSON data to the data.json file
+            file_put_contents('data.json', $jsonData);
+
+            // Return a response to indicate the data was successfully updated
+            $response = array('status' => 'success', 'message' => 'Data updated successfully');
+            echo json_encode($response);
+        } else {
+            // If JSON data is missing in the request, throw an exception
+            throw new Exception('Error: JSON data not found in the request.');
+        }
+    } catch (Exception $e) {
+        // If an exception is caught, echo the error message
+        echo $e->getMessage();
+    }
+}
+
+// Get the location name from the JSON data
+$jsonData = file_get_contents('data.json');
+$dataArray = json_decode($jsonData, true);
+$locationName = isset($dataArray['name']) ? $dataArray['name'] : '';
+
+// Check if the location name is not empty
+if (!empty($locationName)) {
+    // Connect to the MySQL server
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Call the function to retrieve and display weather data
+    displayWeatherData($locationName, $conn);
+
+    // Close the connection
+    $conn->close();
+} else {
+    // If location name is empty, echo an error message
+    echo "Error: Location name not found in the JSON data.";
+}
+?>
+
+
+
+<!-- CREATE TABLE weather_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    location VARCHAR(255) NOT NULL,
+    date DATETIME NOT NULL,
+    weather_main VARCHAR(50) NOT NULL,
+    temperature DECIMAL(5, 2) NOT NULL,
+    humidity INT NOT NULL,
+    pressure INT NOT NULL,
+    wind_speed DECIMAL(5, 2) NOT NULL,
+    UNIQUE KEY unique_location_date (location, date)
+);
+-->
+
